8d54fc9603c8a80f3b6ab9d76e7e00612de1b920
Frank Butler <phranq@gmail.com>
2017-07-18 13:06:26 -0700

    Volunteer model started.

---
+++ VolunteerSystem/Data/VolunteerDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VolunteerSystem.Models;
+
+namespace VolunteerSystem.Data
+{
+    public class VolunteerDbContext : DbContext
+    {
+        public DbSet<Volunteer> Volunteers { get; set; }
+    }
+}
---
+++ VolunteerSystem/Models/Volunteer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VolunteerSystem.Models
+{
+    public class Volunteer
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String userName { get; set; }
+        public String password { get; set; }        // strong hash algorithm expected.
+
+        private ArrayList workCenters = new ArrayList();   // centers where the volunteer prefers to work.
+        private ArrayList interests = new ArrayList();
+
+        public DateTime[] availibility { get; set; }
+
+        public String address { get; set; }
+        public String homeNumber { get; set; }
+        public String workNumber { get; set; }
+        public String cellNumber { get; set; }
+        public String emailAddress { get; set; }
+
+        public String eduBackground { get; set; }
+        private ArrayList curLicenses = new ArrayList();
+
+        public String emergencyName { get; set; }
+        public String emergencyPhone { get; set; }
+        public String emergencyEmail { get; set; }
+        public String emergencyAddr { get; set; }
+
+        public Boolean isLicenseOnFile { get; set; }
+        public Boolean isSSCardOnFile { get; set; }
+
+        public Boolean isApproved { get; set; }
+
+    }
+}
