7db66f3a10d4b8418a5ff4482322750cf7bea291
Frank Butler <phranq@gmail.com>
2017-07-20 20:03:06 -0400

    Broke out more classes to describe the Volunteer.

--- VolunteerSystem/Models/Volunteer.cs
+++ VolunteerSystem/Models/Volunteer.cs
@@ -14,21 +14,21 @@ namespace VolunteerSystem.Models
 
         public string Password { get; set; }
 
-        public virtual ICollection<Center> Centers { get; set; }
-
-        public virtual ICollection<InterestsSkill> InterestsSkills { get; set; }
-
-        public virtual ICollection<Availability> AvailableTimes { get; set; }
-
+        
         public string Address { get; set; }
         public string HomePhone { get; set; }
         public string WorkPhone { get; set; }
         public string CellPhone { get; set; }
         public string Email { get; set; }
-        public string EducationBackground { get; set; }
+        public virtual ICollection<EducationBackground> Background { get; set; }
 
-        public string CurrentLicenses { get; set; }
+        public virtual ICollection<CurrentLicense> Licenses { get; set; }
 
+        public virtual ICollection<Center> Centers { get; set; }
+
+        public virtual ICollection<InterestsSkill> InterestsSkills { get; set; }
+
+        public virtual ICollection<Availability> AvailableTimes { get; set; }
 
     }
 }
