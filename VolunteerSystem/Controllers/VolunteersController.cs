5d8d5f0b7caedd0db851fae186f6e839a70dfd06
citruslogic <phranq@gmail.com>
2017-08-20 16:44:08 -0400

    Fixed Edit action.

--- VolunteerSystem/Controllers/VolunteersController.cs
+++ VolunteerSystem/Controllers/VolunteersController.cs
@@ -142,7 +142,7 @@ namespace VolunteerSystem.Controllers
                 if (ModelState.IsValid)
                 {
                     _context.Add(volunteer);
-            
+
                     await _context.SaveChangesAsync();
                     return RedirectToAction("Index");
                 }
@@ -170,7 +170,10 @@ namespace VolunteerSystem.Controllers
                 return NotFound();
             }
 
-            var volunteer = await _context.Volunteers.SingleOrDefaultAsync(m => m.VolunteerID == id);
+            var volunteer = await _context.Volunteers
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.VolunteerID == id);
+
             if (volunteer == null)
             {
                 return NotFound();
@@ -187,7 +190,7 @@ namespace VolunteerSystem.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,FirstName,LastName,UserName,Password,Address," +
+        public async Task<IActionResult> Edit(int id, [Bind("VolunteerID,FirstName,LastName,UserName,Password,Address," +
             "HomePhone,WorkPhone,CellPhone,Email,LicenseOnFile,SSCardOnFile,Status")] Volunteer volunteer)
         {
             if (id != volunteer.VolunteerID)
@@ -195,94 +198,87 @@ namespace VolunteerSystem.Controllers
                 return NotFound();
             }
 
-            var volunteerToUpdate = await _context.Volunteers.SingleOrDefaultAsync(v => v.VolunteerID == id);
-
-            if (await TryUpdateModelAsync<Volunteer>(
-                volunteerToUpdate,
-                "",
-                v => volunteer.FirstName, v => volunteer.LastName, v => volunteer.UserName, v => volunteer.Password,
-                v => volunteer.Address, v => volunteer.HomePhone, v => volunteer.WorkPhone, v => volunteer.CellPhone,
-                v => volunteer.Email, v => volunteer.LicenseOnFile, v => volunteer.SSCardOnFile, v => volunteer.Status))
-            {
-                try
-                {
-                    await _context.SaveChangesAsync();
-                    return RedirectToAction("Index");
-                }
-                catch (DbUpdateException /* ex */)
-                {
-                    //Log the error (uncomment ex variable name and write a log.)
-                    ModelState.AddModelError("", "Unable to save changes. " +
-                    "Try again, and if the problem persists, " +
-                    "see your system administrator.");
-                }
-            }
-
-            SetViewBagStatusType(volunteer.Status);
-
-            return View(volunteer);
-        }
-
-
-
-        // GET: Volunteers/Delete/5
-        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var volunteer = await _context.Volunteers
-                .AsNoTracking()
-                .SingleOrDefaultAsync(m => m.VolunteerID == id);
-            if (volunteer == null)
-            {
-                return NotFound();
-            }
-            if (saveChangesError.GetValueOrDefault())
-            {
-                ViewData["ErrorMessage"] =
-                "Delete failed. Try again, and if the problem persists " +
-                "see your system administrator.";
-            }
-
-
-            return View(volunteer);
-        }
-
-        // POST: Volunteers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int? id, bool? saveChangesError = false)
-        {
-            var volunteer = await _context.Volunteers
-                .AsNoTracking()
-                .SingleOrDefaultAsync(m => m.VolunteerID == id);
-
-            if (volunteer == null)
-            {
-                return RedirectToAction("Index");
-            }
-
             try
             {
-                _context.Volunteers.Remove(volunteer);
+                _context.Update(volunteer);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
+
 
             }
             catch (DbUpdateException /* ex */)
             {
                 //Log the error (uncomment ex variable name and write a log.)
-                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
-
+                ModelState.AddModelError("", "Unable to save changes. " +
+                "Try again, and if the problem persists, " +
+                "see your system administrator.");
             }
+
+            SetViewBagStatusType(volunteer.Status);
+
+            return View(volunteer);
+    }
+
+
+
+    // GET: Volunteers/Delete/5
+    public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
+    {
+        if (id == null)
+        {
+            return NotFound();
         }
 
-        private bool VolunteerExists(int id)
+        var volunteer = await _context.Volunteers
+            .AsNoTracking()
+            .SingleOrDefaultAsync(m => m.VolunteerID == id);
+        if (volunteer == null)
         {
-            return _context.Volunteers.Any(e => e.VolunteerID == id);
+            return NotFound();
+        }
+        if (saveChangesError.GetValueOrDefault())
+        {
+            ViewData["ErrorMessage"] =
+            "Delete failed. Try again, and if the problem persists " +
+            "see your system administrator.";
+        }
+
+
+        return View(volunteer);
+    }
+
+    // POST: Volunteers/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int? id, bool? saveChangesError = false)
+    {
+        var volunteer = await _context.Volunteers
+            .AsNoTracking()
+            .SingleOrDefaultAsync(m => m.VolunteerID == id);
+
+        if (volunteer == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        try
+        {
+            _context.Volunteers.Remove(volunteer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+        }
+        catch (DbUpdateException /* ex */)
+        {
+            //Log the error (uncomment ex variable name and write a log.)
+            return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+
         }
     }
+
+    private bool VolunteerExists(int id)
+    {
+        return _context.Volunteers.Any(e => e.VolunteerID == id);
+    }
+}
 }
